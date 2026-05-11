5b5bedf016c72a3519fb19496c9078716d8b90ae
Ritesh Sharma <2000riteshsharma@gmail.com>
2026-05-12 01:06:44 +0530

    i have made changes in todo project,changed whole project including HTML,CSS,JS

--- Project/ToDo App/script.js
+++ Project/ToDo App/script.js
@@ -1,26 +1,59 @@
-let input = document.getElementById("input");
-let btn = document.getElementById("btn");
-let list = document.getElementById("list");
+document.addEventListener('DOMContentLoaded', () => {
+  let input = document.getElementById("input");
+  let btn = document.getElementById("btn");
+  let list = document.getElementById("list");
 
-btn.addEventListener("click", () => {
-  let task = input.value;
+  let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 
-  if (task === "") return;
+  tasks.forEach((task) => renderTask(task));
 
-  let li = document.createElement("li");
-  li.innerText = task;
+  btn.addEventListener("click", () => {
+    let task = input.value;
 
-  list.appendChild(li);
+    if (task === "") return;
 
-  input.value = "";
-});
+    const newTask = {
+      id: Date.now(),
+      text: task,
+      completed: false,
+    };
 
-list.addEventListener("click", (e) => {
-  e.target.remove();
-});
+    tasks.push(newTask);
+    saveTask();
+    renderTask(newTask);
+    input.value = "";
 
-input.addEventListener("keydown", (e) => {
-  if (e.key === "Enter") {
-    btn.click();
+  });
+
+  function saveTask() {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
   }
+
+  function renderTask(task) {
+    const li = document.createElement('li');
+    li.setAttribute('data-id', task.id);
+    if (task.completed) li.classList.add('completed');
+    li.innerHTML = `
+    <span>${task.text}</span>
+    <button>delete</button>`;
+
+    li.addEventListener('click', (e) => {
+      if (e.target.tagName === 'BUTTON') return;
+      task.completed = !task.completed;
+      li.classList.toggle('completed');
+      saveTask();
+    });
+
+    li.querySelector('button').addEventListener('click', (e) => {
+      e.stopPropagation();
+      tasks = tasks.filter(t => t.id !== task.id);
+      li.remove();
+      saveTask();
+    })
+    list.appendChild(li);
+  }
+
+  input.addEventListener('keydown', (e) => {
+    if (e.key === "Enter") btn.click();
+  })
 });
